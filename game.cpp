7f60e222c4e9c77526bdcb5358a3184a8e231695
jarreed0 <jarreed0@gmail.com>
2017-09-01 18:27:52 -0500

    header

--- game.cpp
+++ game.cpp
@@ -93,62 +93,15 @@ if(event.type == SDL_KEYDOWN) {
 if(event.type == SDL_KEYUP) {
 }
 }
-
-
 }
 
-void Game::AddController(int id) {
-    if(SDL_IsGameController(id)) {
-        SDL_GameController *pad = SDL_GameControllerOpen(id);
 
-        if( pad ) {
-            SDL_Joystick *joy = SDL_GameControllerGetJoystick(pad);
-            int instanceID = SDL_JoystickInstanceID(joy);
-
-            // You can add to your own map of joystick IDs to controllers here.
-            //YOUR_FUNCTION_THAT_CREATES_A_MAPPING( id, pad );
-        }
+void Game::loadMap(int m) {
+  map.clear();
+  map.empty();
+  if(m == 1) {
+    for(int i=0; i>10; i++) {
+      
     }
-}
-
-void Game::RemoveController(int id) {
-    //SDL_GameController *pad = YOUR_FUNCTION_THAT_RETRIEVES_A_MAPPING( id );
-    SDL_GameControllerClose(pad);
-}
-
-void Game::OnControllerButton(const SDL_ControllerButtonEvent sdlEvent) {
-    // Button presses and axis movements both sent here as SDL_ControllerButtonEvent structures
-}
-
-void Game::OnControllerAxis(const SDL_ControllerAxisEvent sdlEvent) {
-    // Axis movements and button presses both sent here as SDL_ControllerAxisEvent structures
-}
-
-void Game::EventLoop() {
-    SDL_Event sdlEvent;
-
-    while( SDL_PollEvent( &sdlEvent ) ) {
-        switch( sdlEvent.type ) {
-
-        case SDL_CONTROLLERDEVICEADDED:
-            //AddController(sdlEvent.cdevice);
-            break;
-
-        case SDL_CONTROLLERDEVICEREMOVED:
-            RemoveController(sdlEvent.cdevice);
-            break;
-
-        case SDL_CONTROLLERBUTTONDOWN:
-        case SDL_CONTROLLERBUTTONUP:
-            OnControllerButton(sdlEvent.cbutton);
-            break;
-
-        case SDL_CONTROLLERAXISMOTION:
-            OnControllerAxis(sdlEvent.caxis);
-            break;
-
-        // YOUR OTHER EVENT HANDLING HERE
-
-        }
-    }
+  }
 }
--- game.h
+++ game.h
@@ -1,18 +1,12 @@
 #ifndef GAME_H
 #define GAME_H
 
-#ifdef WIN64
-#include <SDL.h>
-#include <SDL_image.h>
-#include <SDL_ttf.h>
-#else
-#include <SDL2/SDL.h>
-#include <SDL2/SDL_image.h>
-#include <SDL2/SDL_ttf.h>
-#endif
+#include ".gitignore/header.h"
 
 #include <iostream>
 using namespace std;
+
+#include <vector>
 
 #include "object.h"
 #include "audio.h"
@@ -27,21 +21,14 @@ public:
   void render();
   void draw(Object o);
   void draw(const char* msg, int x, int y, int r, int g, int b, int size);
-  void AddController(int id);
-  void RemoveController(int id);
-  void OnControllerButton(const SDL_ControllerButtonEvent sdlEvent);
-  void OnControllerAxis(const SDL_ControllerAxisEvent sdlEvent);
-  void EventLoop();
+  void loadMap(int m);
 private:
-  bool block, release;
   SDL_Renderer* ren;
   SDL_Window* win;
   TTF_Font *font;
   bool running;
   int frameCount, timerFPS, lastFrame;
-  SDL_GameController *pad;
-  SDL_Joystick *joy;
-  int instanceID;
+  vector<Object> map;
 };
 
 #endif //GAME_H
